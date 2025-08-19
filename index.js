35cd905badae6cb4a5012e2484f6d4f7596a8e14
Gabriela Goncalvez <gabriela.goncalvez@posteo.net>
2025-08-19 14:09:06 +0200

    dev: bump to v2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World v1 🚀');
+  res.send('Hello World v2 🚀');
 });
 app.listen(process.env.PORT || 8080);
