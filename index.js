5bbcb989006f625b89e78a289586bfaedfbfc41c
Gabriela Goncalvez <gabriela.goncalvez@posteo.net>
2025-09-02 18:01:47 +0200

    tests added, dockerfile and yaml edited to add test step

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 8080;
+
 app.get('/', (req, res) => {
   res.send('Hello World v3 🚀');
 });
-app.listen(process.env.PORT || 8080);
-// bump
-// retry after IAM fix
-// retry after IAM fix
-// retry after IAM fix
-// bump
-// retry after IAM fix
-// retry after IAM fix
-// retry after IAM fix
-// dev test
-// dev test12
+
+// This check ensures the server only starts when the file is run directly
+// and not when it's imported by a test file.
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`Hello World app listening on port ${port}`);
+  });
+}
+
+module.exports = app; // Export for testing
