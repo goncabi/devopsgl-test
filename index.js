94158f969f6fe4d7bfe728e43fd3555d7ecd5290
Gabriela Goncalvez <gabriela.goncalvez@posteo.net>
2025-09-22 16:56:24 +0200

    kleine aenderung

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 8080;
 app.get('/', (req, res) => {
   res.send('Hello World v3 ');
 });
-
+//comment
 // This check ensures the server only starts when the file is run directly
 // and not when it's imported by a test file.
 if (require.main === module) {
