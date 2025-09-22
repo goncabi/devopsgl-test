de48e20f9f7f866d57a3d25ca995ce38536345fa
Lisa <lisa.ruebel@student.htw-berlin.de>
2025-09-22 17:37:58 +0200

    "änderungen zu hello world"

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello World v3 🚀');
+  res.send('Hello World');
 });
 //comment
 // This check ensures the server only starts when the file is run directly
